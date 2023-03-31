d23eaa744928cf83fa1c7b21adcc972e5ea2de8f
Steven Alexandro <c14200044@john.petra.ac.id>
2023-03-31 10:05:04 +0700

    first commit

---
+++ sw.js
@@ -0,0 +1,37 @@
+var CACHE_NAME = 'portfolio-cache-v1';
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open('first-app')
+        .then(function(cache) {
+          cache.addAll([
+            '/',
+            '/index.html',
+            '/styles.css',
+            '/images/*'
+          ])
+        })
+    );
+    return self.clients.claim();
+  });
+
+
+  self.addEventListener("fetch", event => {
+    event.respondWith(
+      caches.match(event.request).then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(response => {
+          if (!response || response.status !== 200 || response.type !== "basic") {
+            return response;
+          }
+          const responseToCache = response.clone();
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, responseToCache);
+          });
+          return response;
+        });
+      })
+    );
+  });
